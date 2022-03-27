2f5814858472035368e5320e202576667edcade3
Dominik Link <Vorbild94@gmail.com>
2022-03-27 11:37:23 +0200

    the answer - structs

--- chapter_5/fish/fish.c
+++ chapter_5/fish/fish.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 
 
-void catalog(const char *name, const char *species, int teeth, int age)
+struct fish {
+    const char *name;
+    const char *species;
+    int teeth;
+    int age;
+};
+
+struct fish snappy = {"Snappy", "Piranha", 69, 4};
+
+void catalog(struct fish fish)
 {
     printf("%s is a %s with %i teeth. He is %i\n",
-           name, species, teeth, age);
+           fish.name, fish.species, fish.teeth, fish.age);
 }
 
-void label(const char *name, const char *species, int teeth, int age)
+void label(struct fish fish)
 {
     printf("Name:%s\nSpecies:%s\n%i years old, %i teeth\n",
-           name, species, teeth, age);
+           fish.name, fish.species, fish.teeth, fish.age);
 }
 
 int main()
 {
-    catalog("Snappy", "Piranha", 69, 4);
-    label("Snappy", "Piranha", 69, 4);
+    catalog(snappy);
+    label(snappy);
     return 0;
 }
